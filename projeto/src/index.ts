d84c59f9f0bd3764d3c4444e54bfd5e5825518bd
farleybsd <farley.t.i@hotmail.com>
2025-05-19 17:08:44 -0300

    Ambiente

---
+++ projeto/src/index.ts
@@ -0,0 +1,9 @@
+ function component() {
+    const element = document.getElementById('content');
+
+    element.innerHTML = "<h1>Hello, World!</h1>";
+    element.innerHTML += "<h1>Hello, World!</h1>";
+    element.innerHTML += "<h1>Hello, World!</h1>";
+    return element;
+  }
+component();
