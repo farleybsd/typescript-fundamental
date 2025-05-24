5b5d64196a05f7ffca32cad79ba367da1b49500a
farleybsd <farley.t.i@hotmail.com>
2025-05-24 11:53:28 -0300

    interface

--- projeto/src/model.ts
+++ projeto/src/model.ts
@@ -5,9 +5,13 @@ export enum accessOptions {
   undefined = 'Não Definido'
 }
 
-export type userType = {
-  fullName: string,
-  register?: string | number,
-  access?: accessOptions,
-  active?: boolean
+interface IPerson {
+  fullName: string;
+  address?: string[];
+}
+
+export interface IUser extends IPerson {
+  register?: string | number;
+  access?: accessOptions;
+  active?: boolean;
 }
